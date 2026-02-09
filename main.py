7751f9fa0b1ac0ae90c5a50fddf0403ec17c6842
loivonmt <lymtrinidad80@gmail.com>
2026-02-09 17:45:55 +0800

    Added section info for Trinidad

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print ("Lab 1: Version Control" )
 print ("Author: LOIS YVONNE M. TRINIDAD" )
 print ("Student ID: TUPM-25-0298" )
-print ("Class Section: BSME 1B")
+print ("Class Section: BSME 1B" )
